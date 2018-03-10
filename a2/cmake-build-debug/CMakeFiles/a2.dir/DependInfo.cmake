38a12e0d2a1772fb228d79ca8b18ac310263e21d
Nicholas Cheng <nickbaicheng@gmail.com>
2018-03-10 14:32:40 -0600

    Start a2

---
+++ a2/cmake-build-debug/CMakeFiles/a2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Git/c++/a2/a2.cpp" "D:/Git/c++/a2/cmake-build-debug/CMakeFiles/a2.dir/a2.cpp.obj"
+  "D:/Git/c++/a2/main.cpp" "D:/Git/c++/a2/cmake-build-debug/CMakeFiles/a2.dir/main.cpp.obj"
+  "D:/Git/c++/a2/trim.cpp" "D:/Git/c++/a2/cmake-build-debug/CMakeFiles/a2.dir/trim.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ a2/cmake-build-debug/CMakeFiles/a2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a2.dir/main.cpp.obj"
+  "CMakeFiles/a2.dir/a2.cpp.obj"
+  "CMakeFiles/a2.dir/trim.cpp.obj"
+  "a2.pdb"
+  "a2.exe"
+  "a2.exe.manifest"
+  "liba2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
